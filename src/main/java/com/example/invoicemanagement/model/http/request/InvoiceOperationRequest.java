6a6b0555b143bb79a0baa45df73e6e69a8a14171
Dimitar Dimitrov <dtdimitrov12@gmail.com>
2025-11-08 14:39:59 +0200

    Allowed editing of issue date

--- src/main/java/com/example/invoicemanagement/model/http/request/InvoiceOperationRequest.java
+++ src/main/java/com/example/invoicemanagement/model/http/request/InvoiceOperationRequest.java
@@ -3,6 +3,7 @@ package com.example.invoicemanagement.model.http.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +13,7 @@ public class InvoiceOperationRequest {
     private Long clientId;
     private String reasonForSkippingVat;
     private List<InvoiceContentOperationRequest> contents;
+    private LocalDate issueDate;
 
     public InvoiceOperationRequest() {
         this.contents = new ArrayList<>();
